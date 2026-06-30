ef61ad71e2a53e714ffd89207019ce2adb244de1
nbeltran <nbeltran@gmail.com>
2026-06-29 23:45:52 -0400

    Se crean componentes y se limpia pagina index

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './pages/inicio/inicio';
+import { Productos } from './pages/productos/productos';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {path: '', component: Inicio},
+  {path: '', component: Productos}
+
+
+
+];
---
+++ src/app/pages/contactanos/contactanos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contactanos',
+  imports: [],
+  templateUrl: './contactanos.html',
+  styleUrl: './contactanos.css',
+})
+export class Contactanos {}
